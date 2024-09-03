4bfa85a62c0af67a8b6587f4613db5e6957f70a1
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-09-03 06:34:24 -0400

    meals

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,23 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Meals = () => {
   const [loadMeals, setLoadMeals] = useState([]);
-  async function fetchMeals() {
-    const response = await fetch("http://localhost:3000/meals");
 
-    if (!response.ok) {
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch("http://localhost:3000/meals");
+
+      if (!response.ok) {
+       
+        return;
+      }
+
+      const meals = await response.json();
+      setLoadMeals(meals);
     }
 
-    const meals = response.json();
-    setLoadMeals(meals);
-  }
+    fetchMeals();
+  }, []);
 
   return (
     <ul id="meals">
       {loadMeals.map((meal) => (
-        <li key={meal.id}>
-           {meal.name} 
-        </li>
+        <li key={meal.id}>{meal.name}</li>
       ))}
     </ul>
   );
